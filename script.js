64cffdbfe447d092783489315a1496a5d77993f7
Barbora Černá <barboracerna267@gmail.com>
2023-11-26 23:12:05 +0100

    final changes

---
+++ script.js
@@ -0,0 +1,25 @@
+/*Napište funkci fillcut, která jako svůj první parametr str očekává řetězec a jako druhý parametr len kladné celé číslo. Úkolem funkce je oříznout nebo prodloužit zadaný řetězec tak, aby měl délku přesně len.
+Pokud je vstupní řetězec delší než len, tak funkce odřízne jeho konec a vrátí výsledek.
+Pokud je vstupní řetězec kratší než len, tak jej doplní od začátku znakem tečky a vrátí výsledek.
+Pokud je vstupní řetězec dlouhý přesně len, funkce jej vrátí beze změny.
+Příklad použití:
+
+document.body.innerHTML += fillcut('petr', 8) + '<br>'; // vypíše „....petr“
+document.body.innerHTML += fillcut('petr', 3) + '<br>'; // vypíše „pet“
+document.body.innerHTML += fillcut('petr', 4) + '<br>'; // vypíše „petr“*/
+
+const fillcut = (str, len) => {
+    if (str.length === len) {
+        return str;
+    } else if (str.length < len ) {
+    const extraDots = len - str.length;
+    const dots = '.'.repeat(extraDots);
+    return dots + str;
+    } else {
+        return str.slice(0,len);
+    }
+}
+
+document.body.innerHTML += fillcut('barbora', 8) + '<br>';
+document.body.innerHTML += fillcut('barbora', 3) + '<br>';
+document.body.innerHTML += fillcut('barbora', 4) + '<br>';
